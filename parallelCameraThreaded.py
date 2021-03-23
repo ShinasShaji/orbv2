9aa76668fcf8b4d44dc5bda05427a3b4d75a27ed
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-03-23 17:33:19 +0530

    Handles parallel cameras using threads; not recommended

---
+++ parallelCameraThreaded.py
@@ -0,0 +1,39 @@
+import cv2
+import threading
+import time
+
+class camThread(threading.Thread):
+    def __init__(self, previewName, camID):
+        threading.Thread.__init__(self)
+        self.previewName = previewName
+        self.camID = camID
+    def run(self):
+        print("Starting " + self.previewName)
+        camPreview(self.previewName, self.camID)
+
+def camPreview(previewName, camID):
+    cv2.namedWindow(previewName)
+    cam = cv2.VideoCapture(camID)
+    cam.set(3, 480)
+    cam.set(4, 240)
+    if cam.isOpened():
+        rval, frame = cam.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow(previewName, frame)
+        time.sleep(0.5)
+        rval, frame = cam.read()
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+    cv2.destroyWindow(previewName)
+
+# Create threads as follows
+thread1 = camThread("Camera 1", 0)
+thread2 = camThread("Camera 2", 2)
+
+thread1.start()
+thread2.start()
+print("\nActive threads", threading.activeCount())
