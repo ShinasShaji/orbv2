55dd26805cbfb3e4b106e43258e5460cd60e5470
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-03-28 20:47:09 +0530

    Captures images in preparation for calibration

--- StereoCam.py
+++ StereoCam.py
@@ -1,9 +1,10 @@
 import cv2
 import time
+import os
 
 class StereoCam:
     """Class to handle a camera pair as a synchronized stereo pair"""
-    def __init__(self, leftID, rightID, width = 540, height = 360, fps=2):
+    def __init__(self, leftID, rightID, width = 640, height = 360, fps=2):
         self.leftCam = cv2.VideoCapture(leftID)
         self.rightCam = cv2.VideoCapture(rightID)
         self.leftCam.set(3, width)
@@ -12,6 +13,10 @@ class StereoCam:
         self.rightCam.set(4, height)
         self.previousTime = time.time()
         self.frameTime = float(1/fps)
+
+        # Capture
+        self.imgCounter = 0
+        self.capturePath = "/home/pi/Documents/orbv2/captures"
 
     def checkOpen(self):
         """Check if the cameras have initialized"""
@@ -34,6 +39,13 @@ class StereoCam:
             return True
         else:
             return False
+
+    def captureImages(self):
+        for (cam, frame) in [("left", self.leftImg), ("right", self.rightImg)]:
+            imgName = "".join([cam, "_{}.png".format(self.imgCounter)])
+            cv2.imwrite(os.path.join(self.capturePath, imgName), frame)
+            print("Captured {}".format(imgName))
+        self.imgCounter += 1
     
     def camPreview(self):
         """Preview the camera outputs"""
@@ -50,10 +62,12 @@ class StereoCam:
                     key = cv2.waitKey(20)
                     if key == 27: # Exit on ESC
                         break
+                    if key == 32: # Capture on SPACE
+                        self.captureImages()
             cv2.destroyAllWindows()
         else:
             print('Could not initialize camera pair')
 
 if __name__=='__main__':
-    Stereo = StereoCam(leftID=0, rightID=2, width=540, height=360, fps=2)
+    Stereo = StereoCam(leftID=0, rightID=2, width=640, height=360, fps=2)
     Stereo.camPreview()
