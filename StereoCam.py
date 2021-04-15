5a6f878dd845e5f3ebf0b5e940912d04bc616ce4
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-04-15 17:03:21 +0530

    StereoCam preview now uses event managed, separate threads

--- StereoCam.py
+++ StereoCam.py
@@ -27,6 +27,11 @@ class StereoCam:
         self.previousTime = time.time()
         self.frameTime = float(1/fps)
 
+        # For event managed capture and preview
+        self.imageEvent = threading.Event()
+        self.quitEvent = threading.Event()
+        self.captureRetVal = True
+
         # Capture details
         self.imgCounter = 0
         self.capturePath = "captures/" 
@@ -51,8 +56,11 @@ class StereoCam:
         retRight, self.rightImg = self.rightCam.retrieve()
 
         if retLeft and retRight:
+            self.captureRetVal = True
             return True
+
         else:
+            self.captureRetVal = False
             return False
 
 
@@ -64,25 +72,41 @@ class StereoCam:
             print("Captured {}".format(imgName))
 
         self.imgCounter += 1
-
     
-    def camPreview_Internal(self):
-        """Preview camera outputs"""
+
+    def startCapture_Internal(self):
         print("Initializing cameras for preview")
         if not self.checkOpen():
             pass
 
-        retVal = self.getFrames()
+        while self.getFrames():
 
-        while retVal:
+            self.imageEvent.set()
+            if self.quitEvent.isSet():
+                break
+
             remainingTime = self.frameTime-(time.time()-self.previousTime)
             if remainingTime<=0:
                 self.previousTime = time.time()
 
-                retVal = self.getFrames()
+            else:
+                time.sleep(remainingTime)
 
+        
+    def startCapture(self):
+        """Runs startCapture_Internal() as a thread"""
+        self.captureThread = threading.Thread(target=self.startCapture_Internal)
+        self.captureThread.start()
+
+    
+    def eventManagedPreview_Internal(self):
+        """Previews capture. Event managed thread"""
+        while self.captureRetVal:
+            if self.imageEvent.wait():
                 cv2.imshow('Left', self.leftImg)
                 cv2.imshow('Right', self.rightImg)
+
+                self.imageEvent.clear()
 
                 key = cv2.waitKey(20)
                 if key == 27: # Exit on ESC
@@ -90,17 +114,21 @@ class StereoCam:
                     break
                 if key == 32: # Capture on SPACE
                     self.captureImages()
-
-            else:
-                time.sleep(remainingTime)
-
+            
         cv2.destroyAllWindows()
+        self.quitEvent.set()
 
     
-    def camPreview(self):
-        """Preview camera outputs. Runs as a thread"""
-        self.previewThread = threading.Thread(target=self.camPreview_Internal)
+    def eventManagedPreview(self):
+        """Runs eventManagedPreview_Internal() as a thread"""
+        self.previewThread = threading.Thread(target=self.eventManagedPreview_Internal)
         self.previewThread.start()
+
+
+    def camPreview(self):
+        """Preview camera outputs"""
+        self.startCapture()
+        self.eventManagedPreview()
 
 
 ### To do: Load stereo calibration data and matrices from jsons ###
