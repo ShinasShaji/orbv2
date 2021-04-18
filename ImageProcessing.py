8441c708d650bd5c494f0ed2fbac8c5a1f831f09
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-04-18 15:17:01 +0530

    Implemented multiprocessing architecture

---
+++ ImageProcessing.py
@@ -0,0 +1,111 @@
+import multiprocessing
+import os
+
+import cv2
+import numpy
+
+
+class ImageProcessing(multiprocessing.Process):
+    """Class to handle image processing on capture stream"""
+    def __init__(self):
+        super(ImageProcessing, self).__init__()
+
+        # Capture details
+        self.imageCounter = 0
+        self.capturePath = "captures/"
+
+        # Flags
+        self.captureBufferReady = False
+        self.captureEventReady = False
+
+
+    def referenceCaptureBuffers(self, buffers, cvImageShape):
+        """Create class references to passed capture buffers and 
+        shape buffers into image arrays"""
+        # Creating class references to buffers
+        assert buffers is not None, "Initialize capture buffers"
+        self.leftImageBuffer = buffers[0]
+        self.rightImageBuffer = buffers[1]
+
+        assert cvImageShape is not None, "Initialize capture buffers"
+        self.cvImageShape = cvImageShape
+
+        # Creating arrays from memory buffers
+        self.leftImage = numpy.frombuffer(self.leftImageBuffer, \
+                            dtype=numpy.uint8).reshape(self.cvImageShape)
+        self.rightImage = numpy.frombuffer(self.rightImageBuffer, \
+                            dtype=numpy.uint8).reshape(self.cvImageShape)
+        
+        self.captureBufferReady = True
+
+    
+    def isCaptureBufferReady(self):
+        if self.captureBufferReady:
+            return True
+        
+        else:
+            print("Capture buffers not referenced")
+            return False
+
+
+    def referenceCaptureEvents(self, events):
+        """Create class references to passed capture events"""
+        self.imageEvent = events[0]
+        self.quitEvent = events[1]
+
+        self.captureEventReady = True
+
+
+    def isCaptureEventReady(self):
+        if self.captureEventReady:
+            return True
+        
+        else:
+            print("Capture events not referenced")
+            return False
+
+
+    def captureImages(self):
+        """Capture and save images from both cameras"""
+        for (cam, frame) in \
+            [("left", self.leftImage), ("right", self.rightImage)]:
+
+            imageName = "".join([cam, "_{}.png".format(self.imgCounter)])
+            cv2.imwrite(os.path.join(self.capturePath, imageName), frame)
+            print("Captured {}".format(imageName))
+
+        self.imageCounter += 1
+
+
+    def capturePreview(self):
+        """Previews capture. Event managed"""
+        if not self.isCaptureBufferReady() or \
+            not self.isCaptureEventReady():
+            pass
+
+        while not self.quitEvent.is_set():
+            if self.imageEvent.wait():
+                cv2.imshow('Left', self.leftImage)
+                cv2.imshow('Right', self.rightImage)
+
+                self.imageEvent.clear()
+
+                key = cv2.waitKey(20)
+                if key == 27: # Exit on ESC
+                    print("Exiting preview")
+                    self.quitEvent.set()
+                    break
+                if key == 32: # Capture on SPACE
+                    self.captureImages()
+            
+        cv2.destroyAllWindows()
+
+
+    def run(self):
+        """Runs when start() is called on this process"""
+        self.capturePreview()
+
+
+
+if __name__=="__main__":
+    print("Import to use")
---
+++ StereoCapture.py
@@ -0,0 +1,165 @@
+import ctypes
+import multiprocessing
+import time
+
+import cv2
+import numpy
+
+
+class StereoCapture(multiprocessing.Process):
+    """Class to handle capture from stereo camera setup"""
+    def __init__(self, cameraID=(2,0), imageSize=(1280,720), fps=2):
+        super(StereoCapture, self).__init__()
+
+        # Camera IDs and properties
+        self.leftID = cameraID[0]
+        self.rightID = cameraID[1]
+        self.imageSize = imageSize
+        self.fps = fps
+
+        # Seting up capture objects
+        self.leftCam = cv2.VideoCapture(self.leftID)
+        self.rightCam = cv2.VideoCapture(self.rightID)
+
+        # Set camera resolution
+        # Left
+        self.leftCam.set(3, self.imageSize[0])
+        self.leftCam.set(4, self.imageSize[1])
+        # Right
+        self.rightCam.set(3, self.imageSize[0])
+        self.rightCam.set(4, self.imageSize[1])
+
+        # Set format to MJPG in FourCC format 
+        self.leftCam.set(cv2.CAP_PROP_FOURCC, \
+            cv2.VideoWriter_fourcc('M','J','P','G'))
+
+        self.rightCam.set(cv2.CAP_PROP_FOURCC, \
+            cv2.VideoWriter_fourcc('M','J','P','G'))
+
+        # Timing
+        self.previousTime = time.time()
+        self.frameTime = float(1/self.fps)
+
+        # Events
+        self.imageEvent = multiprocessing.Event()
+        self.quitEvent = multiprocessing.Event()
+
+        # Flags
+        self.bufferReady = False
+
+
+    def checkOpen(self):
+        """Check if cameras have initialized"""
+        if self.leftCam.isOpened() and self.rightCam.isOpened():
+            return True
+        else:
+            print("Could not initialize camera pair")
+            return False
+
+    
+    def createBuffers(self):
+        """Create and shape buffers into image arrays"""
+        # Check if camera pair has initialized
+        if not self.checkOpen():
+            pass
+
+        # Finding image shape
+        retVal, leftImage = self.leftCam.read()
+        self.cvImageShape = leftImage.shape
+        self.cvImageSize = leftImage.size
+
+        # Creating shared memory buffers
+        self.leftImageBuffer = multiprocessing.Array(ctypes.c_uint8, \
+            self.cvImageSize, lock=False)
+        self.rightImageBuffer = multiprocessing.Array(ctypes.c_uint8, \
+            self.cvImageSize, lock=False)
+
+        # Creating arrays from memory buffers
+        self.leftImage = numpy.frombuffer(self.leftImageBuffer, \
+                            dtype=numpy.uint8).reshape(self.cvImageShape)
+        self.rightImage = numpy.frombuffer(self.rightImageBuffer, \
+                            dtype=numpy.uint8).reshape(self.cvImageShape)
+
+        self.bufferReady = True
+
+    
+    def isBufferReady(self):
+        """Check if buffers have been created"""
+        if self.bufferReady:
+            return True
+        
+        else:
+            print("Buffers not initialized")
+            return False
+
+    
+    def getCVImageShape(self):
+        if self.bufferReady:
+            return self.cvImageShape
+        
+        else:
+            print("Buffers not initialized")
+            return None
+
+    
+    def getBuffers(self):
+        """Returns references of left, right internal buffers"""
+        if self.isBufferReady():
+            return (self.leftImageBuffer, self.rightImageBuffer)
+        
+        else:
+            return None
+
+    
+    def getCaptureEvents(self):
+        """Returns references of image, quit events"""
+        return (self.imageEvent, self.quitEvent)
+
+
+    def getFrames(self):
+        """Grab and retrieve frames"""
+        for i in range(10):
+            self.leftCam.grab()
+            self.rightCam.grab()
+
+        retLeft, self.leftImage[:] = self.leftCam.retrieve()
+        retRight, self.rightImage[:] = self.rightCam.retrieve()
+
+        if retLeft and retRight:
+            return True
+
+        else:
+            return False
+
+
+    def capture(self):
+        """Capture images from camera pair"""
+        print("Initializing cameras for preview")
+        if not self.checkOpen() or not self.isBufferReady():
+            pass
+            
+        while self.getFrames():
+
+            self.imageEvent.set()
+            if self.quitEvent.is_set():
+                break
+
+            remainingTime = self.frameTime-(time.time()-self.previousTime)
+            if remainingTime<=0:
+                self.previousTime = time.time()
+
+            else:
+                time.sleep(remainingTime)
+        
+        self.leftCam.release()
+        self.rightCam.release()
+
+        
+    def run(self):
+        """Runs when start() is called on this process"""
+        self.capture()
+
+
+
+if __name__=="__main__":
+    print("Import to use")
