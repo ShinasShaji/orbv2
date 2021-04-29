a053d48c3f3a5788bd01204cd9a2e82dea6a75aa
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-04-29 18:49:38 +0530

    Keyboar key ASCII lookup

---
+++ helperScripts/Keys.py
@@ -0,0 +1,16 @@
+class Keys:
+    """Lookup table for ASCII values of certain keys"""
+    up = 82
+    down = 84
+    esc = 27
+    space = 32
+    b = 98
+    d = 100
+    k = 107
+    l = 108
+    m = 109
+    n = 110
+    r = 114
+    s = 115
+    u = 117
+    w = 119
