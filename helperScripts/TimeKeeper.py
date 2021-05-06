f3d99c9bc7e7142e92d419b8ffcf2641a73c558e
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-05-06 15:53:15 +0530

    Added method to print elapsed time

--- helperScripts/TimeKeeper.py
+++ helperScripts/TimeKeeper.py
@@ -17,5 +17,12 @@ class TimeKeeper:
         """Stop internal perf_counter and return elapsed time"""
         self.perfCounter[1] = time.perf_counter()
 
-        return (self.perfCounter[1]-self.perfCounter[0])
-        
+        return self.perfCounter[1]-self.perfCounter[0]
+
+    def printPerfCounter(self):
+        """Stop internal perf_counter and print elapsed time"""
+        self.perfCounter[1] = time.perf_counter()
+
+        print("Completed in {:.6f} seconds"\
+                        .format(self.perfCounter[1]-self.perfCounter[0]))      
+  
