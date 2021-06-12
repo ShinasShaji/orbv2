4bcd552bb6c26bd4e38d6a3ee984e3a0d30a0954
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-06-12 13:40:53 +0530

    Implement path collision checking

--- CollisionChecker2D.py
+++ CollisionChecker2D.py
@@ -18,39 +18,91 @@ class Swathcomputation:
 class CollisionChecker2D:
     """Class for 2D Collision Checking"""
     def __init__(self):
-        self.robotFootprint = np.array([0, 0, 20])
+        self.robotFootprint = 200 # x, y, radius
+        self.robotLocation = np.array([0, 0]).astype(np.int16)
+
+        self.voxelGrid = None
+        self.voxelSize = 100
+
+        self.distanceThreshold = int(self.robotFootprint + (self.voxelSize/np.sqrt(2)))
+
+        self.pathFree = True
 
 
     def setLocation(self, location):    
         """Set robot location"""
-        self.robotFootprint[:2] = location[:]
+        self.robotLocation[:2] = location[:]
 
 
     def checkCollision(self, obstacleFootprint):
         """Check collison with obstacle"""    
-        distance = np.linalg.norm(self.robotFootprint[:2]-obstacleFootprint[:2])
+        distance = np.linalg.norm(self.robotLocation[:2]-obstacleFootprint[:2])
         
-        if distance<(self.robotFootprint[2]+obstacleFootprint[2]):
+        if distance<(self.robotFootprint+obstacleFootprint[2]):
             return True
         
         else:
-            return False  
+            return False
+
+
+    def setVoxelGrid(self, voxelGrid):
+        """Set obstacle grid for collision checking"""
+        self.voxelGrid = voxelGrid
+
+
+    def checkPathForCollision(self, path):
+        """Checks given path for collision with voxels in voxelGrid"""
+        self.pathFree = True
+
+        for point in path:
+            distance = np.linalg.norm(self.voxelGrid[:,:2]-point, axis=1).astype(np.int16)
+
+            voxelsInCollision = self.voxelGrid[distance<=self.distanceThreshold]
+
+            if len(voxelsInCollision)>0:
+                self.pathFree = False
+
+                return False, point, voxelsInCollision
+
+            else:
+                continue
+        
+        return True, None, None
+                
 
 
 if __name__=="__main__":
-    obstacleFootprint = np.array([10, 5, 12])
+    #obstacleFootprint = np.array([10, 5, 12])
+
+    #collisionChecker = CollisionChecker2D()
+    #collisionChecker.setLocation([5, 5])
+    #
+
+    #collision = collisionChecker.checkCollision(obstacleFootprint)
+    #if collision:
+    #    print("Collided")
+
+    #else:
+    #    print("Avoided Collision")    
+
+    #swath = Swathcomputation()
+    #swath.setdestination([10,10])   
+    #swath.translation() 
 
     collisionChecker = CollisionChecker2D()
-    collisionChecker.setLocation([5, 5])
-    
 
-    collision = collisionChecker.checkCollision(obstacleFootprint)
-    if collision:
-        print("Collided")
+    path = np.linspace(0, 50, 6).reshape((-1, 1))
+    path = np.hstack((path, np.zeros_like(path)))
+    print("\nPath waypoints: \n", path, "\n")
+
+    voxelGrid = (np.random.randint(100, size=(20, 2)))*10
+    print("Random Voxel Grid: \n", voxelGrid, "\n")
+
+    collisionChecker.setVoxelGrid(voxelGrid)
+    pathFree, collisionObstacle, voxelsInCollision = collisionChecker.checkPathForCollision(path)        
+
+    if pathFree:
+        print("Path free")
 
     else:
-        print("Avoided Collision")    
-
-    swath = Swathcomputation()
-    swath.setdestination([10,10])   
-    swath.translation() 
+        print("Path collision at", collisionObstacle, "with voxels\n", voxelsInCollision)
