d70147c9f53091dc11e9d666c1ea9b1a75e42a00
SiriusBlue5120 <80461084+SiriusBlue5120@users.noreply.github.com>
2021-12-19 01:11:48 +0530

    Leg IK algorithms upto knee, shoulder joints

--- arduinoCode/inverseKinematic/inverseKinematic.ino
+++ arduinoCode/inverseKinematic/inverseKinematic.ino
@@ -2,39 +2,45 @@
 #include <math.h>
 
 
-// May not be necessary, but still
-#define PI 3.1415926535897932384626433832795
-
-
 // Timing
 unsigned int currentTime = 0;               // ms
 unsigned int prevKinematic = 0;             // ms
-unsigned int kinematicsRefreshTime = 10;    // ms
+unsigned int kinematicsRefreshTime = 1000;    // ms
 
 // Leg length parameters
-float legLengths[3] = {55,      // mm; length from hip to shoulder
+float legLengths[4] = {55,      // mm; length from hip to shoulder
                        170,     // mm; length from shoulder to knee
                        170,     // mm; length from knee to foot center
                        5        // mm; foot depth
                        };
 // Note that the parameters above are currently inaccurate
-float legShoulderToFootLength = 0; // mm
-float legLengthHipCompensated = 0; // mm
 
 // Leg endpoint position
-float legEndPointPosition[3] = {0, 100, 55}; // mm; {front, down, outer}
+float legEndPointPosition[3] = {100, 200, 55}; // mm; {back, down, outer}
 
 // Leg angle parameters
 float legAngles[3] = {0, 0, 0};         // {Hip, shoulder, knee}
 float minLegAngles[3] = {-20, 0, 30};
 float maxLegAngles[3] = {40, 90, 165};
-float effectiveShoulderFootAngle = 0;   // rad ?
+
+// Effective and corrected parameters
+// Lengths
+float groundHeight = 0;     // mm
+float legLengthHipCompensated = 0;     // mm
+// Angles
+float effectiveShoulderFootAngle = 0;  // rad
+float shoulderFootProjectionAngle = 0;
+float cosKneeAngle = 0;
+float cosKneeAngleSupplementary = 0;
+float sinKneeAngleSupplementary = 0;
+
 
 
 void setup() {
+  // Starting serial
+  Serial.begin(115200);
+  
   currentTime = millis();
-
-  // Setup servos, serial
 
 }
 
@@ -46,19 +52,54 @@ void loop() {
   // legEndPointPosition is updated here
 
   if ((currentTime - prevKinematic) >= kinematicsRefreshTime) {
-    legShoulderToFootLength = sqrt(pow(legEndPointLocation[0], 2) +
-                                   pow(legEndPointLocation[1], 2));
+    // Update previous kinematics time step
+    prevKinematic = millis();
+    
+    groundHeight = sqrt(pow(legEndPointPosition[0], 2) +
+                        pow(legEndPointPosition[1], 2));
             
-    effectiveShoulderFootAngle = atan2(legEndPointLocation[0],
-                                       legEndPointLocation[1]);
-        
-    // Print values to serial
-    Serial.print("legShoulderToFootLength: ");
-    Serial.println(legShoulderToFootLength);
-        
+    effectiveShoulderFootAngle = atan2(legEndPointPosition[1],
+                                       legEndPointPosition[0]);
+    
+    cosKneeAngle = (pow(legLengths[1], 2) + pow(legLengths[2], 2) - 
+                    pow(legEndPointPosition[0], 2) - pow(legEndPointPosition[1], 2)) / 
+                   (2 * legLengths[1] * legLengths[2]);
+    // Knee angle
+    legAngles[2] = acos(cosKneeAngle);
+    
+    cosKneeAngleSupplementary = - cosKneeAngle;
+    sinKneeAngleSupplementary = sqrt(1 - pow(cosKneeAngleSupplementary, 2));
+    
+    shoulderFootProjectionAngle = atan2(legLengths[2] * sinKneeAngleSupplementary,
+                                        legLengths[1] + legLengths[2] * cosKneeAngleSupplementary);
+
+    // Shoulder angle
+    legAngles[1] = effectiveShoulderFootAngle - shoulderFootProjectionAngle; 
+               
+    // Print values to serial (for testing)
+    Serial.print("groundHeight: ");
+    Serial.println(groundHeight);
+    
     Serial.print("effectiveShoulderFootAngle: ");
     Serial.print(effectiveShoulderFootAngle);
     Serial.print(" ");
     Serial.println(effectiveShoulderFootAngle*180/PI);
+    
+    Serial.print("shoulderFootProjectionAngle: ");
+    Serial.print(shoulderFootProjectionAngle);
+    Serial.print(" ");
+    Serial.println(shoulderFootProjectionAngle*180/PI);
+    
+    Serial.print("legAngles[2](Knee): ");
+    Serial.print(legAngles[2]);
+    Serial.print(" ");
+    Serial.println(legAngles[2]*180/PI);
+    
+    Serial.print("legAngles[1](Shoulder): ");
+    Serial.print(legAngles[1]);
+    Serial.print(" ");
+    Serial.println(legAngles[1]*180/PI);
+        
+    Serial.println("\n");
   }
 }
